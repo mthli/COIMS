e1b6214dfc586574e0d2126a264a18187dc00c8c
Matthew Lee <matthewlee0725@gmail.com>
2014-01-05 11:05:02 +0000

    头文件

---
+++ new.h
@@ -0,0 +1,14 @@
+#ifndef __NEW__
+#define __NEW__
+
+/* 函数库声明部分 */
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+/* 结构体指针部分 */
+
+#endif
